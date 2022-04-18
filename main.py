f83a72f3fb55a9b90c109d0ecf1ddd2357cb241a
beane031@morris.umn.edu <Rubberduck903>
2022-04-18 14:07:25 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+import pandas as pd
+import cv2
+
+
+index = pd.read_csv('index.csv')
+df = index.copy()
+image = cv2.imread('harry-potter/0001/001.jpg')
+image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+cv2.imshow('og', image)
+cv2.imshow('norm', image_norm)
+cv2.imwrite('harry-potter/norm1.jpg', image_norm)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(df.columns)
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
