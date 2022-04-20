7f1a993c511dd00626301e5a48050c0b3b9edfeb
beane031@morris.umn.edu <Rubberduck903>
2022-04-20 13:49:56 -0500

    clean up

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# This is a sample Python script.
 
 import pandas as pd
 import cv2
@@ -19,20 +18,6 @@ for imgPath in df.itertuples():
     cv2.imwrite('norm-' + path, image_norm)
     # print('done')
 
-# image2 = cv2.imread(df['path'])
-# image_norm = cv2.normalize(image2, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
-# cv2.imshow('og', image2)
-# cv2.imshow('norm', image_norm)
-# cv2.imwrite('harry-potter/norm1.jpg', image_norm)
+
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-
-# print(df.columns)
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
