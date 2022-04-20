ec70e5443986f78131501e478afac9b12a483357
beane031@morris.umn.edu <Rubberduck903>
2022-04-20 13:45:33 -0500

    add method to normalize all images

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ import cv2
 
 index = pd.read_csv('index.csv')
 df = index.copy()
-image = cv2.imread('harry-potter/0001/001.jpg')
-image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
-cv2.imshow('og', image)
-cv2.imshow('norm', image_norm)
-cv2.imwrite('harry-potter/norm1.jpg', image_norm)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#image = cv2.imread('harry-potter/0001/001.jpg')
+# print(df['path'])
 
-print(df.columns)
+for imgPath in df.itertuples():
+    path = imgPath[1]
+    # print(path)
+    image = cv2.imread(path)
+    image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+    # print('norm-'+path)
+    # cv2.imshow('norm', image_norm)
+    cv2.imwrite('norm-' + path, image_norm)
+    # print('done')
+
+# image2 = cv2.imread(df['path'])
+# image_norm = cv2.normalize(image2, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+# cv2.imshow('og', image2)
+# cv2.imshow('norm', image_norm)
+# cv2.imwrite('harry-potter/norm1.jpg', image_norm)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# print(df.columns)
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
