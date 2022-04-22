bc608b1ebcfb6d2f73eeb0c1d7c30b8bb440560f
beane031@morris.umn.edu <Rubberduck903>
2022-04-22 13:58:47 -0500

    merge METADATA.csv with training and testing data

--- tensorFlow.py
+++ tensorFlow.py
@@ -9,17 +9,29 @@ import datetime
 import tensorflow_hub as hub
 
 #  Setting up static seed
-static_seed = 420
-np.seed(static_seed)
-tf.set_seed(static_seed)
+#static_seed = 420
+#np.seed(static_seed)
+#tf.set_seed(static_seed)
 
 #  Setting up randomized seed
-# ran_seed = 420
-# random.seed(ran_seed)
-# np.random.seed(ran_seed)
-# tf.random.set_seed(ran_seed)
-
+ran_seed = 420
+random.seed(ran_seed)
+np.random.seed(ran_seed)
+tf.random.set_seed(ran_seed)
 
 TRAIN_DATA = pd.read_csv('index.csv')
 TEST_DATA = pd.read_csv('test.csv')
 METADATA = pd.read_csv('metadata.csv')
+
+print(TRAIN_DATA.shape)
+print(TRAIN_DATA.sample(5))
+
+print(TEST_DATA.shape)
+print(TEST_DATA.sample(5))
+
+print(METADATA.shape)
+print(METADATA.sample(5))
+
+TRAIN_DATA = TRAIN_DATA.merge(METADATA, on = 'class_id')
+TEST_DATA = TEST_DATA.merge(METADATA, on = 'class_id')
+print(TRAIN_DATA.head())
