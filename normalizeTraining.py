b6ee2890497f3009f2bba24d0020b520313316ca
beane031@morris.umn.edu <Rubberduck903>
2022-04-22 13:17:13 -0500

    rename main.py

--- normalizeTraining.py
+++ normalizeTraining.py
@@ -8,11 +8,11 @@ df = index.copy()
 #image = cv2.imread('harry-potter/0001/001.jpg')
 # print(df['path'])
 
-for imgPath in df.itertuples():
+for imgPath in df.itertuples():  # Normalizes all images included in index.csv
     path = imgPath[1]
     # print(path)
     image = cv2.imread(path)
-    image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+    image_norm = cv2.normalize(image, None, alpha=0, beta=200, norm_type=cv2.NORM_MINMAX)
     # print('norm-'+path)
     # cv2.imshow('norm', image_norm)
     cv2.imwrite('norm-' + path, image_norm)
