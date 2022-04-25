51de6ddd407b4c60e5f62a45426c14edaf559688
JacobJenness <jenne077@Morris.umn.edu>
2022-04-25 13:51:55 -0500

    help

--- tensorFlow.py
+++ tensorFlow.py
@@ -6,12 +6,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import random
 import datetime
-import tensorflow_hub as hub
+#import tensorflow_hub as hub
 
 #  Setting up static seed
 #static_seed = 420
-#np.seed(static_seed)
-#tf.set_seed(static_seed)
+# np.seed(static_seed)
+# tf.set_seed(static_seed)
 
 #  Setting up randomized seed
 seed = 420
@@ -33,22 +33,25 @@ print(METADATA.shape)
 print(METADATA.sample(5))
 
 # merge csv files
-TRAIN_DATA = TRAIN_DATA.merge(METADATA, on = 'class_id')
-TEST_DATA = TEST_DATA.merge(METADATA, on = 'class_id')
+TRAIN_DATA = TRAIN_DATA.merge(METADATA, on='class_id')
+TEST_DATA = TEST_DATA.merge(METADATA, on='class_id')
 print(TRAIN_DATA.head())
 
 PATH = '../theoryProject/'
-IMG_SIZE = 256
+IMG_SIZE = 512
 
-def tf_load_data(dataframe, batch_size = 32, img_size = IMG_SIZE, directory_path = PATH, rescale = True):
+
+def tf_load_data(dataframe, batch_size=32, img_size=IMG_SIZE, directory_path=PATH, rescale=True):
     if rescale:
-        dataGenerator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+        dataGenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
     else:
         dataGenerator = tf.keras.preprocessing.image.ImageDataGenerator()
 
-
-    tf_dataset = dataGenerator.flow_from_dataframe(dataframe, directory = directory_path, x_col = 'path', y_col= 'minifigure_name', target_size= (img_size, img_size), batch_size= batch_size,)
+    tf_dataset = dataGenerator.flow_from_dataframe(
+        dataframe, directory=directory_path, x_col='path', y_col='minifigure_name', target_size=(img_size, img_size), batch_size=batch_size,)
     return tf_dataset
+
 
 trainDataset = tf_load_data(TRAIN_DATA)
 testDataset = tf_load_data(TEST_DATA)
@@ -66,7 +69,7 @@ model_mlp = tf.keras.Sequential([
 ], name='MLP')
 
 model_mlp.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
-                  optimizer=tf.keras.optimizers.Adam(learning_rate = 1e-3),
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
                   metrics=['accuracy'])
 
 model_mlp.summary()
@@ -76,26 +79,30 @@ model_mlp.summary()
 # plt.plot(toyTensor)
 # plt.show()
 
+
 def tensorLogging(dir, testName):
     logDir = dir+'/'+testName+'/'+datetime.datetime.now().strftime("%m%d%Y%H%M%S")
     tensorLog = tf.keras.callbacks.TensorBoard(log_dir=logDir)
     print(f"saving yalls logs to: {logDir}")
     return tensorLog
 
+
 checkpoint = 'modelCheckpoint/mlp.ckpt'
 
-
-modelCheckpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint, monitor="val_acc", save_best_only=True,
-    save_weights_only=True, verbose=0)
+# This generates a massive 900mb folder. Delete it before you push to github
+modelCheckpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint, monitor="val_accuracy", save_best_only=True,
+                                                     save_weights_only=True, verbose=0)
 
 #  time to train
 mlpTrain = model_mlp.fit(trainDataset, epochs=12, steps_per_epoch=len(trainDataset), validation_data=testDataset,
-    validation_steps=len(testDataset), callbacks=[tensorLogging('training_logs', 'lego_mlp'), modelCheckpoint])
+                         validation_steps=len(testDataset), callbacks=[tensorLogging('training_logs', 'lego_mlp'), modelCheckpoint])
 
 results = model_mlp.evaluate(testDataset)
 results
 
 #  start plotting MLP performance
+
+
 def plotLossCurves(history):
     loss = history.history['loss']
     valLoss = history.history['val_loss']
@@ -119,6 +126,7 @@ def plotLossCurves(history):
     plt.xlabel('Epochs')
     plt.legend()
     plt.show()
+
 
 plotLossCurves(mlpTrain)
 
