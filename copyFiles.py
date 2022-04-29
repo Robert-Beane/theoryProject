57738b189f365cb72d0b043aca66c8dfff535c92
beane031@morris.umn.edu <beane031@morris.umn.edu>
2022-04-29 17:47:45 -0500

    Edit to make easier

--- copyFiles.py
+++ copyFiles.py
@@ -3,12 +3,11 @@ import os
 
 # function based off: https://pynative.com/python-copy-files-and-directories/
 
-src = '../theoryProject/marvel/0001/'
-des = '../theoryProject/copy/'
+src = '../theoryProject/other/0004/'
 
 for file in os.listdir(src):
     source = src + file
-    destination = des + "0" + file
+    destination = src + "0" + file
     if os.path.isfile(source):
         shutil.copy(source, destination)
         print('copied', file)
